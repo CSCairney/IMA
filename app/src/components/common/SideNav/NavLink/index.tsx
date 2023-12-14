f6de7fe6b6e643147ee3f417fb7af918409c370a
Charles Cairney <charlescairney@yahoo.co.uk>
2023-12-14 13:06:03 +0000

    Update metadata and arrow icons in SideNav

--- app/src/components/common/SideNav/NavLink/index.tsx
+++ app/src/components/common/SideNav/NavLink/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import { NavLink } from "../types/nav";
-import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 type NavlinkProps = {
     name: string;
@@ -24,18 +24,20 @@ const Navlink: React.FC<NavlinkProps> = ({ name, path, subPaths }) => {
                 <Link href={path} className={styles.link}>
                     {name}
                 </Link>
-                {subPaths ? subPathsOpen ? <FaArrowAltCircleUp className={styles.subPathToggle} onClick={() => toggleSubPaths()} /> : <FaArrowAltCircleDown className={styles.subPathToggle} onClick={() => toggleSubPaths()} /> : null}
+                {subPaths ? subPathsOpen ? <IoIosArrowUp className={styles.subPathToggle} onClick={() => toggleSubPaths()} /> : <IoIosArrowDown className={styles.subPathToggle} onClick={() => toggleSubPaths()} /> : null}
             </div>
             {subPaths && subPathsOpen && (
-                <ul>
-                    {subPaths.map((subPath, index) => (
-                        <li key={index}>
-                            <Link href={subPath.path} className={styles.link}>
-                                {subPath.name}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <div className={styles.subpaths}>
+                    <ul className={styles.subPathList}>
+                        {subPaths.map((subPath, index) => (
+                            <li key={index} className={styles.link}>
+                                <Link href={subPath.path} className={styles.link}>
+                                    {subPath.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             )}
         </div>
     );
